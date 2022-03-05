664b9816e8dd47c80bf7d5c0eb74b0f61b90a964
nagolove <dekar.own@gmail.com>
2022-03-05 23:55:41 +0300

    try to implement several experiments.

--- stat1.lua
+++ stat1.lua
@@ -16,25 +16,30 @@ https://youtu.be/I2u5PfQRtJo?t=4516
 
 local rand = math.random
 
---math.randseed()
 
-print(rand())
-print(rand())
-print(rand())
-print(rand())
+--print(rand())
+--print(rand())
+--print(rand())
+--print(rand())
 
-local EXPERIMENTS_NUM = 100000
+local EXPERIMENTS_NUM = 1000
 local bank = 0.
 
-for i = 1, EXPERIMENTS_NUM do
-    local p = rand()
-    print("i, p, bank", i, p, bank)
-    -- Важно-ли, что не захватывается точное значение 2/3 в условии?
-    if p > 0 and p < 2/3 then
-        bank = bank + 2
-    elseif p > 2/3 then
-        bank = bank - 3
+--for j = 1, 10 do 
+    --print(os.time())
+    math.randomseed(os.time())
+    for i = 1, EXPERIMENTS_NUM do
+        local p = rand()
+        print("i, p, bank", i, p, bank)
+        -- Важно-ли, что не захватывается точное значение 2/3 в условии?
+        if p > 0 and p < 2/3 then
+            bank = bank + 2
+        elseif p > 2/3 then
+            ----bank = bank - 3
+            bank = bank - 3.5
+            --bank = bank - 4
+        end
     end
-end
+--end
 
 print('bank', bank)
